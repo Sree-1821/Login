a885efbf86d5960f9205314cf639ac8982f87587
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2022-12-14 05:58:02 +0000

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,54 @@
+﻿using LOG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LOG.Models;
+using System.Web.Security;
+
+namespace LOG.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Log(Login MODEL,string returnUrl)
+        {
+            LoginPage1Entities db= new LoginPage1Entities();
+            var dataItem=db. Logins.Where(X=>X.UserName == MODEL.UserName && X.Password==MODEL.Password).First();
+            if (dataItem != null)
+            {
+                FormsAuthentication.SetAuthCookie(dataItem.UserName, false);
+                if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("\\"))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid user/pass");
+                return View();
+            }
+                
+
+                }
+            }
+        }
+
+
+    
